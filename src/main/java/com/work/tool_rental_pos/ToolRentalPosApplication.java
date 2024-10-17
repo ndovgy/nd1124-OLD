d1924f194dbe8d9becc69b0a0971be53fe8522c3
Natalia Dovgy <ndovgy@yahoo.com>
2024-10-17 14:03:38 -0400

    Initial commit

---
+++ src/main/java/com/work/tool_rental_pos/ToolRentalPosApplication.java
@@ -0,0 +1,13 @@
+package com.work.tool_rental_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolRentalPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolRentalPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/work/tool_rental_pos/ToolRentalPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.tool_rental_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolRentalPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
